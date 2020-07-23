1429c4a83baffe09da56e8e3f433b00e393a46ee
Emily Pillmore <emilypi@cohomolo.gy>
2020-07-23 11:36:17 -0400

    update nix configs

---
+++ smash-optics/default.nix
@@ -0,0 +1,28 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, optics-core, smash, stdenv }:
+      mkDerivation {
+        pname = "smash-optics";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base optics-core smash ];
+        homepage = "https://github.com/emilypi/smash";
+        description = "Optics for the `smash` library using `optics-core`";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
